8dc0c7b6d86199e71e1928b0a529e77813915f9b
Arithmetics <brock.m.tillotson@gmail.com>
2018-01-29 13:20:39 -0800

    refactoring route

--- app/components/WeatherDay.js
+++ app/components/WeatherDay.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import kToF from '../utils/helpers';
+import moment from 'moment';
 
 
 class WeatherDay extends React.Component {
@@ -6,12 +8,16 @@ class WeatherDay extends React.Component {
 
   render(){
     const iconUrl="../../app/images/weather-icons/" + this.props.iconName + ".svg";
+    kToF(300);
     return (
       <div  className="weathercard">
-        <img src={iconUrl} alt="weather"></img>
-        <h4>{this.props.time}</h4>
+        <div className="imageholder">
+          <img src={iconUrl} alt="weather"></img>
+        </div>
+        <h4>{moment(this.props.time).format("MMMM Do")}</h4>
+        <h4>{moment(this.props.time).format("h:mm a")}</h4>
         <h5>{this.props.description}</h5>
-        <h5>{this.props.temperature}</h5>
+        <h5>{kToF(this.props.temperature)} &deg; F</h5>
       </div>
     )
   }
---
+++ app/utils/helpers.js
@@ -0,0 +1,9 @@
+function kToF(kelvin){
+  return Math.round(kelvin * (9/5) -459.67, 1)
+}
+
+function test(){
+  console.log('lallal')
+}
+
+export default kToF;
