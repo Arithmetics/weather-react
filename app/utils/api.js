076b5c43da9c1fef20f84e5acd0843a6e601e6b8
Arithmetics <brock.m.tillotson@gmail.com>
2018-01-29 16:45:28 -0800

    using https for request now

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,12 +1,12 @@
 async function fetchCurrentWeather(location){
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&type=accurate&APPID=cd5636cbefe61df414ec932089509492`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&type=accurate&APPID=cd5636cbefe61df414ec932089509492`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
 async function fetchFiveDayForecast(location){
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=cd5636cbefe61df414ec932089509492`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=cd5636cbefe61df414ec932089509492`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
